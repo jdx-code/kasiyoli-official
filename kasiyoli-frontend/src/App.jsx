d0d5db6b970be53c0c596e34e19e4b716c4d668a
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-08-06 13:33:35 +0530

    Installed Tailwind and creating the navbar

--- kasiyoli-frontend/src/App.jsx
+++ kasiyoli-frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Menu from './components/Menu'
 import './App.css'
 
 function App() {
+
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
   const [listOfUsers, setListOfUsers] = useState([])
 
   useEffect(() => {
@@ -13,6 +17,38 @@ function App() {
 
   return (
     <>
+
+      <nav>
+        <div className="max-w-7xl mx-auto flex items-center justify-between h-20 px-4">
+          <div className="flex-shrink-0 font-bold tracking-wider">
+            <p>Kasiy7oli</p>
+          </div>
+          <div className="hidden md:block">
+            <Menu />
+          </div>
+          <button
+            type="button"
+            className="md:hidden bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+            onClick={() => setShowMobileMenu(!showMobileMenu)}>
+            <svg
+              className="h-6 w-6"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className="md:hidden">
+          {showMobileMenu && <Menu />}
+        </div>
+      </nav>
+
       <div>
         {listOfUsers.map((user) => {
           return (
