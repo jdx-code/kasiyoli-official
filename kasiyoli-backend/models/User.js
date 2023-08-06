9de96e38af990ef329610647f941ed8af80fd86d
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-08-06 22:58:15 +0530

    Installed passport for authentication and bcrypt for encryption

--- kasiyoli-backend/models/User.js
+++ kasiyoli-backend/models/User.js
@@ -1,20 +1,34 @@
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    userName: { 
-        type: String, 
-        required: true,
-        unique: true,    
-    },
-    email: { 
-        type: String,
-        required: true, 
-        unique: true, 
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+  userName: { type: String, unique: true },
+  email: { type: String, unique: true },
+  password: String
 })
 
-module.exports = mongoose.model('users', UserSchema)
+
+// Password hash middleware.
+ 
+ UserSchema.pre('save', function save(next) {
+  const user = this
+  if (!user.isModified('password')) { return next() }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err) }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) { return next(err) }
+      user.password = hash
+      next()
+    })
+  })
+})
+
+
+// Helper method for validating user's password.
+UserSchema.methods.comparePassword = function comparePassword(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+
+
+module.exports = mongoose.model('User', UserSchema)
