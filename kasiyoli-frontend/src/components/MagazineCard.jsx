cbd7a6407cc9e4eaf4c6f0b95dcbec4a0c75e41c
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-08-20 21:03:04 +0530

    Testing cards..

--- kasiyoli-frontend/src/components/MagazineCard.jsx
+++ kasiyoli-frontend/src/components/MagazineCard.jsx
@@ -2,6 +2,9 @@ const MagazineCards = () => {
 
     return (                                                  
         <>
+            <div className="card">
+                <p>Cards go here..</p>
+            </div>
         </>        
     )
 }
