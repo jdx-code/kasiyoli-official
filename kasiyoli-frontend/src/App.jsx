34a1a283a9ebf06e17e4fef7752e80843238c528
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-09-13 14:51:55 +0530

    Testing

--- kasiyoli-frontend/src/App.jsx
+++ kasiyoli-frontend/src/App.jsx
@@ -21,6 +21,7 @@ function App() {
 
     return (
       <>
+        {/* Routes */}
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
