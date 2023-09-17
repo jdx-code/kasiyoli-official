252cffda76a2b8b519e51a54925d229c2dfc5f49
AnkurZS <ankurgoswami1912@gmail.com>
2023-09-17 20:25:28 +0530

    fixing form issues

--- kasiyoli-frontend/src/pages/Admin/SubCategory.jsx
+++ kasiyoli-frontend/src/pages/Admin/SubCategory.jsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
-import Sidebar from '../../components/Sidebar'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Sidebar from '../../components/Sidebar';
 
-const SubCategory = () => {
+function SubCategory() {
     const [formData, setFormData] = useState({
-            categoryName: "",
-            subCategory: "", 
-            subCategoryDesc: ""
-        }
-    )
+        categoryName: "",
+        subCategory: "", 
+        subCategoryDesc: ""
+    })
 
     const [categories, setCategories] = useState([]); // Store categories here
 
@@ -25,7 +24,7 @@ const SubCategory = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        Axios.post('http://localhost:5000/admin/add-subcategory', {
+        axios.post('http://localhost:5000/admin/add-subcategory', {
             categoryName: formData.categoryName,
             subCategory: formData.subCategory,
             subCategoryDesc: formData.subCategoryDesc
@@ -33,7 +32,7 @@ const SubCategory = () => {
     }
 
     const getSubCategory = () => {
-        Axios.get('http://localhost:5000/admin/sub-category')
+        axios.get('http://localhost:5000/admin/sub-category')
         .then((res) => {
             setData(res.data)
         })
@@ -41,7 +40,7 @@ const SubCategory = () => {
 
     useEffect(() => {
         // Fetch categories from the server when the component mounts
-        Axios.get('http://localhost:5000/admin/category')
+        axios.get('http://localhost:5000/admin/category')
         .then((res) => {
             setCategories(res.data);
         })
@@ -50,66 +49,84 @@ const SubCategory = () => {
         });
     }, []); // Empty dependency array to run the effect only once
 
-    return (
-        <div class="row">
-            <div class="col-sm-2">
-                <Sidebar />
-            </div>
-            <div class="col-md-10">
-                   
-            <form onSubmit={handleSubmit}>
-                <select
-                    onChange={handleChange}
-                    name="categoryName"
-                    value={formData.categoryName}
-                    >
-                    <option value="">Select a category</option>
-                    {categories.map((category) => (
-                        <option key={category._id} value={category._id}>
-                        {category.categoryName}
-                        </option>
-                    ))}
-                </select>
-                <input 
-                    type='text'
-                    placeholder='Sub Category'
-                    onChange={handleChange}
-                    name="subCategory"
-                    value={formData.subCategory}
-                />
-                <input 
-                    type='text'
-                    placeholder='Sub-Category Description'
-                    onChange={handleChange}
-                    name="subCategoryDesc"
-                    value={formData.subCategoryDesc}
-                />
-                <button>Submit</button>
-            </form>
+  const myStyle = {
+    margin: "0 auto",
+    width: "50%"     
+  }
 
-            <button onClick={getSubCategory}>Get SubCategory</button>
-            <div>
-                {data.length > 0 ? (
-                    data.map((dataObj) => (
-                        <div key={dataObj._id}>
-                            <p>Category Name: {dataObj.category.categoryName}</p>
-                            <p>Sub Category Name: {dataObj.subCategory}</p>
-                            <p>Sub Category Description: {dataObj.subCategoryDesc}</p>
-                        </div>
-                    ))
-                ) : (
-                    <p>No data available.</p>
-                )} 
-            </div>
+  return (
+    <div className="row">
+      <div className="col-sm-2">
+        <Sidebar />
+      </div>
+      <div className="container my-12">
+      <div className="col-md-10">
+        <form onSubmit={handleSubmit} style={myStyle}>
+          <div className="form-group">
+            <label htmlFor="categoryName">Category Name:</label>
+            <select
+              className="form-control"
+              onChange={handleChange}
+              name="categoryName"
+              value={formData.categoryName}
+            >
+              <option value="">Select a category</option>
+              {categories.map((category) => (
+                <option key={category._id} value={category._id}>
+                  {category.categoryName}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="subCategory">Sub Category:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Sub Category"
+              onChange={handleChange}
+              name="subCategory"
+              value={formData.subCategory}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="subCategoryDesc">Sub-Category Description:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Sub-Category Description"
+              onChange={handleChange}
+              name="subCategoryDesc"
+              value={formData.subCategoryDesc}
+            />
+          </div><br />
+          <button className="btn btn-primary">
+            Submit
+          </button>
+        </form><br />
+        <hr /> <br />
+        <div style={myStyle}>
+            <button className="btn btn-secondary" onClick={getSubCategory}>
+            Get SubCategory
+            </button>
+        </div><br />
+        <div style={myStyle}>
+          {data.length > 0 ? (
+            data.map((dataObj) => (
+              <div key={dataObj._id} className="mt-3">
+                <p>Category Name: {dataObj.category.categoryName}</p>
+                <p>Sub Category Name: {dataObj.subCategory}</p>
+                <p>Sub Category Description: {dataObj.subCategoryDesc}</p>
+              </div>
+            ))
+          ) : (
+            <p>No data available.</p>
+          )}
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
 
-            </div>
-         </div>
-
-        
-        
-
-      
-    )
-    }
-
-export default SubCategory
+export default SubCategory;
