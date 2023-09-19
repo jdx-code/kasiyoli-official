aa5f4189531e2a67cb7d7d335084e12341802265
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-09-19 13:48:59 +0530

    Added data to staticDB

--- kasiyoli-frontend/src/pages/Admin/PostManager.jsx
+++ kasiyoli-frontend/src/pages/Admin/PostManager.jsx
@@ -1,19 +1,32 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import Sidebar from '../../components/Sidebar';
 
-export default function PostManager() {
-  const editorRef = useRef(null);
-
-  const [formData, setFormData] = useState({
-    postTitle: '',
-    category: '',
-    subCategory: '',
-    volume: '',
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
+};
+const PostManager = () => {
+  // state
+  const [state, setState] = useState({
+    title: "",
+    category: "okCat",
+    subCategory: "okSubCat",
+    volume: "okVol",
   });
-
-  const [editorContent, setEditorContent] = useState('');
+  const [content, setContent] = useState("");
 
   const [categories, setCategories] = useState([]);
   const [subCategories, setSubCategories] = useState([]);
@@ -52,47 +65,45 @@ export default function PostManager() {
       });
   }, []);
 
-  const handleEditorChange = (content, editor) => {
-    setEditorContent(content);
+  // rich text editor handle change
+  const handleContent = (event) => {
+    console.log(event);
+    setContent(event);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // destructure values from state
+  const { title, category, subCategory, volume } = state;
 
-    try {
-      const response = await axios.post('http://localhost:5000/admin/add-post', {
-        postTitle: formData.postTitle,
-        category: formData.category,
-        subCategory: formData.subCategory,
-        volume: formData.volume,
-        postContent: editorContent,
-      });
-
-      console.log(response.data);
-
-      // Clear the form fields after submission
-      setFormData({
-        postTitle: '',
-        category: '',
-        subCategory: '',
-        volume: '',
-      });
-      setEditorContent('');
-    } catch (error) {
-      console.error('Error sending data to the backend', error);
-    }
+  // onchange event handler
+  const handleChange = (name) => (event) => {
+    // console.log('name', name, 'event', event.target.value);
+    setState({ ...state, [name]: event.target.value });
   };
 
-  const handleChange = (event) => {
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [event.target.name]: event.target.value,
-      };
-    });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // console.table({ title, content, user });
+    axios
+      .post(
+        `http://localhost:5000/admin/add-post`,
+        { title, category, subCategory, volume, content },        
+      )
+      .then((response) => {
+        console.log(response);
+        // empty state
+        setState({ ...state, title: "", category: "", subCategory: "", volume: "", content: "" });
+        setContent("");
+        // show success alert
+        alert(`${response.data.title} blog post is created`);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        alert(error.response.data.error);
+      });
   };
 
   return (
+
     <>
       <div className="row">
         <div className="col-sm-2">
@@ -107,8 +118,8 @@ export default function PostManager() {
                   type="text"
                   className="form-control"
                   name="postTitle"
-                  onChange={handleChange}
-                  value={formData.postTitle}
+                  onChange={handleChange("title")}
+                  value={title}
                   placeholder='Post Title'
                 />
               </div>
@@ -116,9 +127,9 @@ export default function PostManager() {
                 <label htmlFor="category">Category:</label>
                 <select
                   className="form-control"
-                  onChange={handleChange}
+                  onChange={handleChange("category")}
                   name="category"
-                  value={formData.category}
+                  value={category}
                 >
                   <option value="">Select a category</option>
                   {categories.map((cat) => (
@@ -132,9 +143,9 @@ export default function PostManager() {
                 <label htmlFor="subCategory">Sub Category:</label>
                 <select
                   className="form-control"
-                  onChange={handleChange}
+                  onChange={handleChange("subCategory")}
                   name="subCategory"
-                  value={formData.subCategory}
+                  value={subCategory}
                 >
                   <option value="">Select a sub-category</option>
                   {subCategories.map((subcat) => (
@@ -148,9 +159,9 @@ export default function PostManager() {
                 <label htmlFor="volume">Volume:</label>
                 <select
                   className="form-control"
-                  onChange={handleChange}
+                  onChange={handleChange("volume")}
                   name="volume"
-                  value={formData.volume}
+                  value={volume}
                 >
                   <option value="">Select a volume</option>
                   {volumes.map((vol) => (
@@ -162,26 +173,14 @@ export default function PostManager() {
               </div>
               <div className="form-group">
                 <label>Post Content:</label>
-                <Editor
-                  onInit={(evt, editor) => (editorRef.current = editor)}
-                  value={editorContent}
-                  onEditorChange={handleEditorChange}
-                  init={{
-                    height: 500,
-                    menubar: false,
-                    plugins: [
-                      'advlist autolink lists link image charmap print preview anchor',
-                      'searchreplace visualblocks code fullscreen',
-                      'insertdatetime media table paste code help wordcount',
-                    ],
-                    toolbar:
-                      'undo redo | formatselect | ' +
-                      'bold italic backcolor | alignleft aligncenter ' +
-                      'alignright alignjustify | bullist numlist outdent indent | ' +
-                      'removeformat | help',
-                    content_style:
-                      'body { font-family: Helvetica, Arial, sans-serif; font-size: 14px }',
-                  }}
+                <ReactQuill
+                  modules={modules}
+                  onChange={handleContent}
+                  value={content}
+                  theme="snow"
+                  className=""
+                  placeholder="Write something.."
+                  style={{ border: "1px solid yellow" }}
                 />
               </div>
               <button className="btn btn-primary">
@@ -192,5 +191,8 @@ export default function PostManager() {
         </div>
       </div>
     </>
+    
   );
-}
+};
+
+export default PostManager;
