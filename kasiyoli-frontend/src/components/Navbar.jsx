0bf5ca2115ef5a9f187075b71978038193e2f8da
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-09-21 16:17:26 +0530

    Fixed responsive navbar

--- kasiyoli-frontend/src/components/Navbar.jsx
+++ kasiyoli-frontend/src/components/Navbar.jsx
@@ -44,7 +44,30 @@ const Navbar = (props) => {
       },      
   ]
 
-    const [showMobileMenu, setShowMobileMenu] = useState(false);     
+  const [showMobileMenu, setShowMobileMenu] = useState(false);   
+    
+    // Function to generate navigation links
+  const generateNavLinks = (links) => {
+    return (
+      <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 text-[#fef8ef] font-medium">
+        <NavLink 
+          className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+          to="/"
+        > 
+          Home
+        </NavLink>
+        {links.map(({ linkName, to }) => (
+          <NavLink 
+            className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            key={to} 
+            to={to}
+          >
+            {linkName}
+          </NavLink>
+        ))}
+      </div>
+    );
+  };
 
     return(
 
@@ -53,40 +76,11 @@ const Navbar = (props) => {
           <div className="flex-shrink-0 font-bold tracking-wider">
             <p>Kasiy7oli</p>
           </div>
+          
           <div className="hidden md:block">
-            <div className="px-2 md:px-0 py-3 space-y-2 md:space-y-0 md:space-x-2 text-[#fef8ef] font-medium"> 
-                <NavLink 
-                    to="/"
-                    className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                >
-                    Home
-                </NavLink>  
-                {props.links === "homeLinks" ? (
-                     homeLinks.map(({linkName, to}) => {
-                      return(
-                        <NavLink 
-                          to={to}
-                          className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                      >
-                          {linkName}
-                      </NavLink>
-                      )
-                    })  
-                ) : (
-                  magazinelinks.map(({linkName, to}) => {
-                    return(
-                      <NavLink 
-                        to={to}
-                        className="block md:inline-block px-3 py-2 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                    >
-                        {linkName}
-                    </NavLink>
-                    )
-                  })
-                )}
-                
-            </div>
+            {generateNavLinks(props.links === 'homeLinks' ? homeLinks : magazinelinks)}
           </div>
+          
           <button
             type="button"
             className="md:hidden bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
@@ -106,7 +100,7 @@ const Navbar = (props) => {
           </button>
         </div>
         <div className="md:hidden">
-          {showMobileMenu && <Navbar />}
+          {showMobileMenu && generateNavLinks(props.links === 'homeLinks' ? homeLinks : magazinelinks)}
         </div>
       </nav>
     )
