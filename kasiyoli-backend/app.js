b93d93d1e5bf34e226aa6e8249a8048193a5f2af
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-09-23 14:32:29 +0530

    Added file size limit in app.js

--- kasiyoli-backend/app.js
+++ kasiyoli-backend/app.js
@@ -20,6 +20,8 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use(express.json({ limit: '10mb' })); // Increase the limit as needed
+
 app.use('/', homeRoutes)
 app.use('/admin', adminRoutes)
 
