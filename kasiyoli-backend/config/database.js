8072418617920830eb218da15fafb723166d560a
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-09-25 14:33:13 +0530

    database connection string renamed

--- kasiyoli-backend/config/database.js
+++ kasiyoli-backend/config/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(`${process.env.MONGODB_URI}`, {
+        const url = process.env.MONGO_URL
+        const conn = await mongoose.connect(url, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         })
